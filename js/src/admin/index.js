215c3333523af49dbf93abf87004844a5d6188ff
Ziven <admin@49.232.237.81>
2026-08-01 17:33:33 +0800

    v0.1.0b: 修 '初始化失败' 错误
    - 改 admin/index.js 用 app.registry.for('ziven-bjxy-website').registerPage(BjxySettings)
    - 替代错误的 app.extensionSettings[...] = ... (Flarum 2.0 不存在 extensionSettings API)
    - 正确 API 参考 ziven-dress-up 模式 (registerPage 让 ExtensionPage 框架渲染 settings page)
    - 部署 admin.js 含 5 处 registerPage 调用

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,8 +1,11 @@
 // ziven-bjxy-website admin entry
-// 注册 /admin#/extension/ziven-bjxy-website 页面
+// 注册 /admin#/extension/ziven-bjxy-website 页面 (Flarum 2.0 registry 模式)
+// v0.1.0a 修: app.extensionSettings 在 Flarum 2.0 不存在, 改用 app.registry.for('xxx').registerPage()
 import app from 'flarum/admin/app';
 import BjxySettings from './components/BjxySettings';
 
 app.initializers.add('ziven-bjxy-website', () => {
-  app.extensionSettings['ziiven-bjxy-website'] = () => m(BjxySettings);
+  app.registry
+    .for('ziven-bjxy-website')
+    .registerPage(BjxySettings);
 });
